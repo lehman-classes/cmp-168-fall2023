2ba21c5ec76d952ff7981a9ba3907f0d8ede25c3
Hendrix Tavarez <htavarez@gmail.com>
2023-11-14 21:30:27 -0500

    recursive

---
+++ ch15/App.java
@@ -0,0 +1,12 @@
+class App {
+
+  public static void printHello(String name) {
+    System.out.printf("Hello, %s\n", name);
+  }
+
+  public static void main(String[] args) {
+
+    printHello("everyone!");
+
+  }
+}
