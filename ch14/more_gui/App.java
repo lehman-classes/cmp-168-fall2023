348153a800073e4d93890c6781cd6fecdcb60991
Hendrix Tavarez <htavarez@gmail.com>
2023-11-16 19:10:53 -0500

    proj2 gui stuff

--- ch14/more_gui/App.java
+++ ch14/more_gui/App.java
@@ -1,3 +1,5 @@
+// video link here top of class
+
 import javax.swing.JFrame;
 import java.awt.Color;
 import javax.swing.JMenuBar;
@@ -10,6 +12,38 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 class App {
+
+  public static JPanel createPanel(int rows, int cols) {
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(rows, cols));
+    int totalCells = rows * cols;
+    for (int i = 0; i < totalCells; i++) {
+      JButton button = new JButton();
+
+      // instead of setting the text to the index of the button, it should be set to
+      // either 0 if not bombs around or the number of bombs around
+      // you might need to call another function to get the number of bombs around
+      // you also need to check if there is a bomb in the current cell. if there is a
+      // bomb.
+      button.setActionCommand(String.valueOf(i));
+
+      button.setBackground(Color.LIGHT_GRAY);
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e) {
+          String command = e.getActionCommand();
+          // still need to check if there is a bomb in the current cell
+          // if there is a bomb, then the game is over
+          // also, you need to check if there are no more cells to click (You won!, if no
+          // more cells to click)
+          button.setText(command);
+          button.setBackground(Color.CYAN);
+          button.setEnabled(false);
+        }
+      });
+      panel.add(button);
+    }
+    return panel;
+  }
 
   public static void main(String[] args) {
 
@@ -34,22 +68,7 @@ class App {
     frame.setJMenuBar(menuBar);
     frame.getContentPane().setBackground(Color.YELLOW);
 
-    JPanel panel = new JPanel();
-    panel.setLayout(new GridLayout(5, 5));
-    for (int i = 0; i < 25; i++) {
-      JButton button = new JButton();
-      button.setActionCommand(String.valueOf(i));
-      button.setBackground(Color.LIGHT_GRAY);
-      button.addActionListener(new ActionListener() {
-        public void actionPerformed(ActionEvent e) {
-          String command = e.getActionCommand();
-          button.setText(command);
-          button.setBackground(Color.CYAN);
-          button.setEnabled(false);
-        }
-      });
-      panel.add(button);
-    }
+    JPanel panel = createPanel(5, 5);
     frame.add(panel);
 
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
